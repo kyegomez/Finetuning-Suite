c5fa02366ab20567c26f31722dacac6d2c1f0fb0
Kye <kye@apacmediasolutions.com>
2023-09-04 09:38:54 -0400

    ft

--- finetuning_suite/processing/build_dataset.py
+++ finetuning_suite/processing/build_dataset.py
@@ -1,67 +1,56 @@
-#builds dataset automatically => adds to your hf account
-import multiprocessing
 import argparse
+import multiprocessing
 from itertools import chain
+
 from datasets import load_dataset
-from transformers import AutoTokenizer
 
-class CFG:
-    SEED: int = 42
-    SEQ_LEN: int = 8192 # context length make it larger or smaller depending on your task
-    NUM_CPU: int = multiprocessing.cpu_count()
-    HF_ACCOUNT_REPO: str = "YOUR HF ACCOUNT"
-    #ADD YOUR OWN TOKENIZER
-    TOKENIZER: str = "EleutherAI/gpt-neox-20b"
-    #ADD YOUR OWN DATASET
-    DATASET_NAME: str = "EleutherAI/the_pile_deduplicated"
+from kosmosx.model import KosmosTokenizer
 
-def build_dataset(args):
-    tokenizer = AutoTokenizer.from_pretrained(CFG.TOKENIZER)
-    train_dataset = load_dataset(CFG.DATASET_NAME, split="train")
 
-    #ADD YOUR OWN TOKENIZE LOGIC
-    def tokenize_function(example):
-        return tokenizer([t + tokenizer.eos_token for t in example["text"]])
+class BuildDataset:
+    def __init__(self, seed=42, seq_len=8192, hf_account="YOUR HUGGINGFACE API KEY", dataset_name="uggingFaceM4/VQAv2"):
+        self.SEED = seed
+        self.SEQ_LEN = seq_len
+        self.NUM_CPU = multiprocessing.cpu_count()
+        self.HF_ACCOUNT_REPO = hf_account
+        self.DATASET_NAME = dataset_name
+        self.tokenizer = KosmosTokenizer.tokenize
 
-    tokenized_dataset = train_dataset.map(
-        tokenize_function,
-        batched=True,
-        num_proc=CFG.NUM_CPU,
-        remove_columns=["text"],
-    )
+    def tokenize_function(self, example):
+        return self.tokenizer([t + self.tokenizer.eos_token for t in example["text"]])
 
-    block_size = CFG.SEQ_LEN
-
-    # Main data processing function that will concatenate all texts from our dataset and generate chunks of block_size.
-    def group_texts(examples):
-        # Concatenate all texts.
+    def group_texts(self, examples):
         concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
         total_length = len(concatenated_examples[list(examples.keys())[0]])
-        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-        # customize this part to your needs.
-        if total_length >= block_size:
-            total_length = (total_length // block_size) * block_size
-        # Split by chunks of max_len.
+        if total_length >= self.SEQ_LEN:
+            total_length = (total_length // self.SEQ_LEN) * self.SEQ_LEN
         result = {
-            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+            k: [t[i : i + self.SEQ_LEN] for i in range(0, total_length, self.SEQ_LEN)]
             for k, t in concatenated_examples.items()
         }
         return result
 
-    train_tokenized_dataset = tokenized_dataset.map(
-        group_texts,
-        batched=True,
-        num_proc=CFG.NUM_CPU,
-    )
-
-    train_tokenized_dataset.push_to_hub(CFG.HF_ACCOUNT_REPO)
+    def build(self):
+        train_dataset = load_dataset(self.DATASET_NAME, split="train", streaming=True)
+        tokenized_dataset = train_dataset.map(
+            self.tokenize_function,
+            batched=True,
+            num_proc=self.NUM_CPU,
+            remove_columns=["text"],
+        )
+        train_tokenized_dataset = tokenized_dataset.map(
+            self.group_texts,
+            batched=True,
+            num_proc=self.NUM_CPU,
+        )
+        train_tokenized_dataset.push_to_hub(self.HF_ACCOUNT_REPO)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Process and push dataset to Hugging Face Hub")
-    parser.add_argument("--seed", type=int, default=CFG.SEED, help="Random seed")
-    parser.add_argument("--seq_len", type=int, default=CFG.SEQ_LEN, help="Sequence length for processing")
-    parser.add_argument("--hf_account", type=str, default=CFG.HF_ACCOUNT_REPO, help="Hugging Face account name and repo")
-    parser.add_argument("--tokenizer", type=str, default=CFG.TOKENIZER, help="Tokenizer model to use")
-    parser.add_argument("--dataset_name", type=str, default=CFG.DATASET_NAME, help="Name of the dataset to process")
+    parser.add_argument("--seed", type=int, default=42, help="Random seed")
+    parser.add_argument("--seq_len", type=int, default=8192, help="Sequence length for processing")
+    parser.add_argument("--hf_account", type=str, default="YOUR HUGGINGFACE API KEY", help="Hugging Face account name and repo")
+    parser.add_argument("--dataset_name", type=str, default="uggingFaceM4/VQAv2", help="Name of the dataset to process")
     args = parser.parse_args()
-    build_dataset(args)
+    dataset_builder = BuildDataset(seed=args.seed, seq_len=args.seq_len, hf_account=args.hf_account, dataset_name=args.dataset_name)
+    dataset_builder.build()
