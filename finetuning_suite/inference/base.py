99adab8a5d3e2e8ecb2bf0a15b0ed76ee9222927
Kye <kye@apacmediasolutions.com>
2023-09-07 14:13:04 -0400

    self device

--- finetuning_suite/inference/base.py
+++ finetuning_suite/inference/base.py
@@ -8,7 +8,7 @@ class InferenceHandler(ABC):
 
 class DefaultInferenceHandler(InferenceHandler):
     def run(self, prompt_text, model, tokenizer, device, max_length):
-        inputs = tokenizer.encode(prompt_text, return_tensors="pt").to(device)
+        inputs = tokenizer.encode(prompt_text, return_tensors="pt").to(self.device)
         outputs = model.run(inputs, max_length=max_length, do_sample=True)
         return tokenizer.decode(outputs[0], skip_special_tokens=True)
     
